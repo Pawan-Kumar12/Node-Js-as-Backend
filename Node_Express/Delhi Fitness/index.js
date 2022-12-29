1a1f111a3ef8a2202d5f3a5aed0d86ad2b66a8ec
Pawan-Kumar12 <pk1215tr@gmail.com>
2022-12-29 12:21:07 +0530

    comment added regarding port 80

--- Node_Express/Delhi Fitness/index.js
+++ Node_Express/Delhi Fitness/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const port = 80;
+const port = 80;  //perks of using 80 is we don't need to write port no after localhost 
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
