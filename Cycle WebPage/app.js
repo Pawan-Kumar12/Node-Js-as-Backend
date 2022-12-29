9ca9a345e6a16b61a851c8f77dc8d5a00397294f
Pawan-Kumar12 <pk1215tr@gmail.com>
2022-12-29 19:33:53 +0530

    Earlier created Cycle webpage is rebuild using pug and form inputted data is stored at local in formData.txt file using Node Js

---
+++ Cycle WebPage/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res)=>{
+    Name = req.body.Name
+    Email = req.body.Email
+    Contact = req.body.Contact
+    Intro = req.body.Intro
+    console.log(req.body);
+
+    let outputToWrite = `
+
+    Name: ${Name}, 
+    Email: ${Email},
+    Contact: ${Contact},
+    Intro: ${Intro}. `;
+    
+    console.log(fs.existsSync('formData.txt'));
+
+    if(!fs.existsSync('formData.txt')){
+        fs.writeFileSync('formData.txt', outputToWrite);
+    }
+    else
+    {
+        fs.appendFileSync('formData.txt',outputToWrite);
+    }
+
+
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+})
+
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
