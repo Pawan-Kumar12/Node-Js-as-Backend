75c17f2ea6cd7b25d74c1a471999ab2200285cdc
Pawan-Kumar12 <pk1215tr@gmail.com>
2022-12-29 12:18:04 +0530

    Using Node Js as back-end lang for our Delhi Fitness website to store user inputted form data into a text file at our local

---
+++ Node_Express/Delhi Fitness/index.js
@@ -0,0 +1,60 @@
+//This Js code will store user inputted form data into an file named formData.txt using Express js, PUG etc
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())  //To extract the data from the website to the index.js file
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+//End point
+app.get('/', (req, res)=>{
+    const params = {'title': 'Delhi Fitness Club'}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res)=>{
+    Name = req.body.Name
+    Age = req.body.Age
+    Gender = req.body.Gender
+    Address = req.body.Address
+    Email = req.body.Email
+    Contact_No = req.body.Contact_No
+    console.log(req.body);
+
+    let outputToWrite = `
+
+    Name: ${Name}, 
+    Age: ${Age},
+    Gender: ${Gender},
+    Email: ${Email}, 
+    Contact: ${Contact_No}. `;
+    
+    console.log(fs.existsSync('formData.txt'));
+
+    if(!fs.existsSync('formData.txt')){
+        fs.writeFileSync('formData.txt', outputToWrite);
+    }
+    else
+    {
+        fs.appendFileSync('formData.txt',outputToWrite);
+    }
+
+
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+})
+
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+
