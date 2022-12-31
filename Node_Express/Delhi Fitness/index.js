dbf5ff8e0367e6e783b73c37c6718fbde1ba6ef5
Pawan-Kumar12 <pk1215tr@gmail.com>
2023-01-01 01:35:05 +0530

    Delhi Fitness website's form fillimg method is connected to server using mongodb cluster

--- Node_Express/Delhi Fitness/index.js
+++ Node_Express/Delhi Fitness/index.js
@@ -1,9 +1,35 @@
 //This Js code will store user inputted form data into an file named formData.txt using Express js, PUG etc
 const express = require("express");
+var MongoClient = require('mongodb').MongoClient;
 const path = require("path");
 const fs = require("fs");
 const app = express();
 const port = 80;  //perks of using 80 is we don't need to write port no after localhost 
+const uri = "mongodb+srv://itspk:pkPK321pk@cluster0.azkxsd2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+
+ //Related to mongoDB Cluster
+ async function main(){
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();  
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+};
+
+ async function dataUpload(client, Info) {
+   const UserInfo = await client.db("DelhiFitness").collection("GymData").insertOne(Info);
+    console.log("UserInfo data :",UserInfo);
+};
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -20,21 +46,24 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/', (req, res)=>{
-    Name = req.body.Name
-    Age = req.body.Age
-    Gender = req.body.Gender
-    Address = req.body.Address
-    Email = req.body.Email
-    Contact_No = req.body.Contact_No
-    console.log(req.body);
+    main().catch(console.error);
 
+    const Info = ({   
+        Name : req.body.Name,
+        Age : req.body.Age,
+        Gender : req.body.Gender,
+        Address : req.body.Address,
+        Email : req.body.Email,
+        Contact_No : req.body.Contact_No,
+    });
+// Data to store in formData file(not required just including as different functionality)
     let outputToWrite = `
 
-    Name: ${Name}, 
-    Age: ${Age},
-    Gender: ${Gender},
-    Email: ${Email}, 
-    Contact: ${Contact_No}. `;
+    Name: ${Info.Name}, 
+    Age: ${Info.Age},
+    Gender: ${Info.Gender},
+    Email: ${Info.Email}, 
+    Contact: ${Info.Contact_No}. `;
     
     console.log(fs.existsSync('formData.txt'));
 
@@ -46,15 +75,7 @@ app.post('/', (req, res)=>{
         fs.appendFileSync('formData.txt',outputToWrite);
     }
 
-
-    const params = {'message': 'Your form has been submitted successfully'}
-    res.status(200).render('index.pug', params);
-})
-
-
-
-// START THE SERVER
-app.listen(port, ()=>{
-    console.log(`The application started successfully on port ${port}`);
-});
-
+      
+     dataUpload(client, Info);
+    res.status(200).render('index.pug');
+ });
